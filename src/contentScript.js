5d560492473e03a7dc29d3473099709f219a7464
Baptiste Lalanne <61800805+BaptisteLalanne@users.noreply.github.com>
2022-04-12 14:56:52 +0200

    :building_construction: Add React and bundler
    * :building_construction: Setup new architecture
    
    * :construction: Add React to the project
    
    * :construction: Add scss
    
    * :construction: Great start
    
    * :construction: gitignone
    
    * :sparkles: Clean archi

---
+++ src/contentScript.js
@@ -0,0 +1,21 @@
+/*global chrome*/
+
+const messagesFromReactAppListener = (message, sender, response) => {
+  console.log('[content.js]. Message received', {
+      message,
+      sender,
+  })
+
+  if (
+      sender.id === chrome.runtime.id &&
+      message.message === 'Hello from React') {
+      response('Hello from content.js');
+  }
+
+}
+
+/**
+* Fired when a message is sent from either an extension process or a content script.
+*/
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
+

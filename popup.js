7da6aafaa6455b02456c1e6d0e32cdf22aa2e944
GrannySmith21 <bastien.barbe65@gmail.com>
2022-04-12 08:09:35 +0200

    Innit cookie auto-delete after a while

--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
+chrome.windows.onCreated.addListener(function() {
+    chrome.windows.getAll(function(windows) {
+        if (windows.length == 1) {
+            console.log("open");
+        }
+    });
+});
+
 document.getElementById("getCookies").addEventListener("click", async function(){
 	console.log("Hello")
 	//const cookieStorage = await chrome.cookies.getAllCookieStores();
@@ -6,31 +14,4 @@ document.getElementById("getCookies").addEventListener("click", async function()
 	  domain: "<all_urls>"
 	});
 	console.log(cookies)
-	const cookies2 = await chrome.cookies.getAll({
-	  domain: "google.com"
-	});
-	console.log(cookies2)
-	const cookies3 = await chrome.cookies.getAll({
-	  domain: ""
-	});
-	console.log(cookies3)
-	const cookies4 = await chrome.cookies.getAll({
-	  domain: ".fr"
-	});
-	console.log(cookies4)
-	const cookies5 = await chrome.cookies.getAll({
-	  domain: ".com"
-	});
-	console.log(cookies5)
-	/*cookies.forEach(cookie => {
-		console.log(cookie);
-	})*/
-
-	/*chrome.cookies.getAll({
-	  domain: "<all_urls>"
-	}, function (cookies) {
-	  for (var i = 0; i < cookies.length; i++) {
-	    console.log(cookies[i] + "found");
-	  }
-	});*/
 });

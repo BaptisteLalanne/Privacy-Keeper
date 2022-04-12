5a8c71e1a546b2ad7d4eee1cdd9bdd3d25fad05f
Baptiste Lalanne <baptiste.lalanne@outlook.fr>
2022-04-12 12:55:27 +0200

    :construction: Add React to the project

--- public/background.js
+++ public/background.js
@@ -1,9 +1,4 @@
-'use strict';
-
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+/*global chrome*/
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {

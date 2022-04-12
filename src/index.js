84aefbcdb9ede001afa5b413814937d5b61ab408
Baptiste Lalanne <baptiste.lalanne@outlook.fr>
2022-04-12 13:00:52 +0200

    :construction: Add scss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.scss';
 
+// Needed file to render React
+// Do not touch
 ReactDOM.render(
   <React.StrictMode>
     <App />

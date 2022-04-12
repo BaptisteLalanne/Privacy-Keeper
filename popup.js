356a4c82d6e6cc56efe607e8f5e248b033658b55
Arthur Durand <62608501+ArthurDurand@users.noreply.github.com>
2022-04-12 08:27:50 +0200

    Ajout background .js avec listener ouverture browser

--- popup.js
+++ popup.js
@@ -1,10 +1,3 @@
-chrome.windows.onCreated.addListener(function() {
-    chrome.windows.getAll(function(windows) {
-        if (windows.length == 1) {
-            console.log("open");
-        }
-    });
-});
 
 document.getElementById("getCookies").addEventListener("click", async function(){
 	console.log("Hello")

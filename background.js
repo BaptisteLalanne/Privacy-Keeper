1bb50f30a03d498a41652bdc77afd74ea810a074
Arthur Durand <62608501+ArthurDurand@users.noreply.github.com>
2022-04-13 11:24:36 +0200

    Delete unused cookies fonctionnel

--- background.js
+++ background.js
@@ -1,44 +1,67 @@
 //Listener sur ouverture browser
 chrome.windows.onCreated.addListener(function() {
-	chrome.storage.sync.get("updateDateCookies", function(result) {
+	//On recupere nos donnees
+	chrome.storage.local.get("updateDateCookies", async function(result) {
 
-		max_diff = 1;
+		//console.log(result)
 
+		//duree a partir de laquelle on delete un cookie (en ms)
+		//a set dans settings (via storage)
+		max_diff = 1000*60*60*24*7*2; //2 semaines
+
+		//les donnees recuperees
 		if(result && result["updateDateCookies"])
 			result = result["updateDateCookies"];
 		else 
 			result = {};
+
+		value = {}
+
+		//date d'aujourd'hui (en ms)
 		date_now = Date.now().toString();
 
-		chrome.cookies.getAll({}, function(cookies){
+		//On recupere les cookies
+		await chrome.cookies.getAll({}).then(cookies => {
 			cookies.forEach(cookie => {
 				key = "domain"+cookie.domain+"name"+cookie.name;
-				if(result[key] && result[key]-date_now > max_diff){
-					console.log("Max_diff reached! : "+result[key]-date_now);
-					//remove
+
+				//Si on n'a pas de date pour le cookie das le storage on l'ajoute
+				if(!result[key]){
+					//console.log("cookieDate set")
+					//console.log(key)
+					value[key] = date_now;
+				}
+				//On check si le cookie est inutilisé depuis trop longtemps
+				else if(result[key] && date_now-result[key] > max_diff){
+					//console.log("Max_diff reached! : ");
+					//console.log(cookie)
+					//Delete cookie (url = domain ? idk how to get CookieDetails)
+					chrome.cookies.remove({"name":cookie.name, "storeId":cookie.storeId, "url":"https://"+cookie.domain+cookie.path/*, url:cookie.domain*/}, function(details){
+						delete result[key]
+						console.log("Cookie deleted")
+						if (chrome.runtime.lastError) {
+							console.log("Runtime error.");
+							//console.log(key)
+							//console.log(cookie)
+						}
+						//console.log("Cookie deleted")
+					});
+				} 
+				//Si ca fait moins de max_diff
+				else {
+					value[key] = date_now
 				}
 			})
-		});
+		}).catch(err => console.log(err));
 
+		//On met les données modifiées dans le storage
+    	await chrome.storage.local.set({"updateDateCookies": value}).then(() => {
+			if(chrome.runtime.error) {
+				console.log("Runtime error.");
+			}
+		}).catch(err => console.log(err));
 	});
-
-	/*
-	const cookiesPromise = chrome.cookies.getAll({});
-	cookiesPromise.then(logCookies)
-	function logCookies(cookies){
-		cookies.forEach(cookie => {
-			key = "domain"+cookie.domain+"name"+cookie.name
-			// delete key if date - now > dateMax
-		})
-		//console.log(cookies)
-	}
-	console.log("Here")*/
 });
-
-//Check for changes (cookies)
-/*chrome.cookies.onChanged.addListener(function(changeinfo){
-
-})*/
 
 //Permet d'update la date de dernière mise à jour des cookies
 //Listener sur active tab
@@ -48,71 +71,6 @@ chrome.tabs.onUpdated.addListener(setInfos)
 
 
 function setInfos(activeInfo){
-
-
-	//On recupere nos donnees
-	chrome.storage.local.get("updateDateCookies", async function(result) {
-
-		console.log(result)
-
-		//duree a partir de laquelle on delete un cookie (en ms)
-		//a set dans settings (via storage)
-		max_diff = 1000*10; //10 secondes
-
-		//les donnees recuperees
-		if(result && result["updateDateCookies"])
-			result = result["updateDateCookies"];
-		else 
-			result = {};
-
-		console.log(typeof result)
-
-		//date d'aujourd'hui (en ms)
-		date_now = Date.now().toString();
-
-		console.log(result)
-
-		//On recupere les cookies
-		await chrome.cookies.getAll({}).then(cookies => {
-			console.log("doDeleteCookies")
-			console.log(cookies)
-			cookies.forEach(cookie => {
-				key = "domain"+cookie.domain+"name"+cookie.name;
-
-				//Si on n'a pas de date pour le cookie das le storage on l'ajoute
-				if(!result[key]){
-					//console.log("cookieDate set")
-					//console.log(key)
-					result[key] = date_now;
-				}
-				//On check si le cookie est inutilisé depuis trop longtemps
-				else if(result[key] && date_now-result[key] > max_diff){
-					//console.log("Max_diff reached! : ");
-					//console.log(cookie)
-					//Delete cookie (url = domain ? idk how to get CookieDetails)
-					chrome.cookies.remove({"name":cookie.name, "storeId":cookie.storeId, "url":"https://"+cookie.domain+cookie.path/*, url:cookie.domain*/}, function(details){
-						delete result[key]
-						if (chrome.runtime.lastError) {
-							console.log("Runtime error.");
-							//console.log(key)
-							//console.log(cookie)
-						}
-						//console.log("Cookie deleted")
-					});
-				}
-			})
-		}).catch(err => console.log(err));
-
-		//On met les données modifiées dans le storage
-    	await chrome.storage.local.set({"updateDateCookies": result}).then(() => {
-    		console.log(result)
-    		console.log("Set cookies")
-			if(chrome.runtime.error) {
-				console.log("Runtime error.");
-			}
-		}).catch(err => console.log(err));
-	});
-
 	
 	//On query la tab active (avec le listener on a simplement le tabId & windowId)
 	let queryOptions = { active: true, currentWindow: true };
@@ -142,6 +100,9 @@ function setInfos(activeInfo){
 						value = result["updateDateCookies"]
 					else 
 						value = {}
+
+					console.log("result")
+					console.log(value)
 					//On insère les cookies dans la value
 					date_now = Date.now().toString()
 					cookies.forEach(cookie => {
@@ -162,84 +123,9 @@ function setInfos(activeInfo){
 			});
 		}
 	});
+
+	chrome.cookies.getAll({},function(cookies){
+		console.log("All cookies :")
+		console.log(cookies)
+	})
 };
-
-/*chrome.tabs.onActivated.addListener(function(activeInfo){
-	let queryOptions = { active: true, currentWindow: true };
-	chrome.tabs.query(queryOptions, function(tabs){
-		console.log(tabs)
-		console.log(tabs[0].url)
-
-		chrome.cookies.getAll({"url":tabs[0].url},function(cookies){
-
-			chrome.storage.sync.get("updateDateCookies", function(result) {
-				
-			  console.log('Value currently is ');console.log(result);
-			});
-
-	        console.log(cookies);
-	        value = {}
-	        cookies.forEach(cookie => {
-	        	key = "domain"+cookie.domain+"name"+cookie.name
-	        	value[key] = Date.now().toString()
-	        });
-	        console.log("Value : ")
-	        console.log(value)
-	        //value = "hello"
-	        //chrome.storage.sync.set({"updateDateCookies": JSON.stringify(value)}, function() {
-	        console.log({"updateDateCookies": value})
-	       	chrome.storage.sync.set({"updateDateCookies": value}, function() {
-				if (chrome.runtime.error) {
-					console.log("Runtime error.");
-				}
-				console.log("success")
-			});
-
-			chrome.storage.sync.get("updateDateCookies", function(result) {
-			  console.log('Value currently is ');console.log(result);
-			});
-
-			chrome.storage.sync.get(null, function(result) {
-			  console.log('Value currently is (from null) ');console.log(result);
-			});
-		});
-	});
-});*/
-
-/*
-chrome.storage.sync.set({key: value}, function() {
-  console.log('Value is set to ' + value);
-});
-
-chrome.storage.sync.get(['key'], function(result) {
-  console.log('Value currently is ' + result.key);
-});*/
-
-//Scripting
-/*chrome.scripting.executeScript({
-	    target: { tabId: activeInfo.tabId},
-	    function: getDocumentCookies,
-	});
-function getDocumentCookies() {
-   console.log("hello")
-   console.log(document.cookie)
-   document.cookie
-}*/
-
-//get active tabs & cookies
-/*
-chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRENT,"active":true}, function(tab){
-            console.log(JSON.stringify(tab));
-            chrome.cookies.getAll({"url":tab[0].url},function (cookie){
-                console.log(cookie.length);
-                allCookieInfo = "";
-                for(i=0;i<cookie.length;i++){
-                    console.log(JSON.stringify(cookie[i]));
-
-                    allCookieInfo = allCookieInfo + JSON.stringify(cookie[i]);
-                }
-                localStorage.currentCookieInfo = allCookieInfo;
-            });
-    });
-*/
-

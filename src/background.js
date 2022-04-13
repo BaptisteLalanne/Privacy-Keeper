897f001a5eb1d7f9b27938656132c8c8ab3d1f77
GrannySmith21 <bastien.barbe65@gmail.com>
2022-04-13 13:55:36 +0200

    :bulb: french to english

--- src/background.js
+++ src/background.js
@@ -8,16 +8,16 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     }
 });
 
-//Listener sur ouverture browser
+//Listen when the browser is opened
 chrome.windows.onCreated.addListener(function () {
-    //On recupere nos donnees
+    //Getting data
     chrome.storage.local.get("updateDateCookies", async function (result) {
 
-        //duree a partir de laquelle on delete un cookie (en ms)
-        //a set dans settings (via storage)
-        let max_diff = 1000 * 60 * 60 * 24 * 7 * 2; //2 semaines
+        //Time after which unused cookies are deleted
+        //To be set in settings
+        let max_diff = 1000 * 60 * 60 * 24 * 7 * 2; //2 weeks
 
-        //les donnees recuperees
+        //data fetched
         if (result && result["updateDateCookies"])
             result = result["updateDateCookies"];
         else
@@ -25,22 +25,21 @@ chrome.windows.onCreated.addListener(function () {
 
         let value = {};
 
-        //date d'aujourd'hui (en ms)
         let date_now = Date.now().toString();
 
-        //On recupere les cookies
+        //Getting all the cookies
         await chrome.cookies.getAll({}).then(cookies => {
             let key;
             cookies.forEach(cookie => {
                 key = "domain" + cookie.domain + "name" + cookie.name;
 
-                //Si on n'a pas de date pour le cookie das le storage on l'ajoute
+                //If we don't have a date in the storage for the cookie we add it
                 if (!result[key]) {
                     value[key] = date_now;
                 }
-                //On check si le cookie est inutilisé depuis trop longtemps
+                //We check if the cookie hasn't been used for too long
                 else if (result[key] && date_now - result[key] > max_diff) {
-                    //Delete cookie (url = domain ? idk how to get CookieDetails)
+                    //Delete cookie
                     chrome.cookies.remove({
                         "name": cookie.name,
                         "storeId": cookie.storeId,
@@ -52,14 +51,14 @@ chrome.windows.onCreated.addListener(function () {
                         }
                     });
                 }
-                //Si ca fait moins de max_diff
+                //If lower than max_diff
                 else {
-                    value[key] = date_now;
+                    value[key] = result[key];
                 }
             })
         }).catch(err => console.log(err));
 
-        //On met les données modifiées dans le storage
+        //We put the now upodated cookies' date in the storage
         await chrome.storage.local.set({"updateDateCookies": value}).then(() => {
             if (chrome.runtime.error) {
                 console.log("Runtime error.");
@@ -68,42 +67,40 @@ chrome.windows.onCreated.addListener(function () {
     });
 });
 
-//Permet d'update la date de dernière mise à jour des cookies
-//Listener sur active tab
+//To update the last time a cookie was used
+//Listen to new tabs
 chrome.tabs.onActivated.addListener(setInfos);
-//Listener sur update tab (changement d'url par ex)
+//Listener to updated tabs (when the url is modifies for instance)
 chrome.tabs.onUpdated.addListener(setInfos);
 
 
 function setInfos() {
 
-    //On query la tab active (avec le listener on a simplement le tabId & windowId)
+    //Query the active tab
     let queryOptions = {active: true, currentWindow: true};
     chrome.tabs.query(queryOptions, function (tabs) {
         if (tabs.length > 0 && tabs[0].url !== "") {
 
-            //On récupère la totalité des cookies lié à l'url de la page
+            //Getting all the cookie whose url matches the active tab
             chrome.cookies.getAll({"url": tabs[0].url}, function (cookies) {
 
-                //On récupère les données déjà présentes
+                //Getting stored cookies' dates
                 chrome.storage.local.get("updateDateCookies", function (result) {
                     let value;
-                    //on recupère les données si elles sont présentes
+                    // Getting them only if they exist
                     if (result && result["updateDateCookies"])
                         value = result["updateDateCookies"];
                     else
                         value = {};
 
-                    console.log("result");
-                    console.log(value);
-                    //On insère les cookies dans la value
+                    //Updating cookies' dates
                     let date_now = Date.now().toString()
                     let key;
                     cookies.forEach(cookie => {
                         key = "domain" + cookie.domain + "name" + cookie.name;
                         value[key] = date_now;
                     });
-                    //On met la value dans le storage
+                    //Putting the new date into the local storage
                     chrome.storage.local.set({"updateDateCookies": value}, function () {
                         if (chrome.runtime.error) {
                             console.log("Runtime error.");
@@ -113,9 +110,4 @@ function setInfos() {
             });
         }
     });
-
-    chrome.cookies.getAll({}, function (cookies) {
-        console.log("All cookies :");
-        console.log(cookies);
-    })
 }

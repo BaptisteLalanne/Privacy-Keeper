36cd999663352d2095d1656ded5c00620a295e9a
Marcus Toma <best4tdm@gmail.com>
2022-04-14 10:42:42 +0200

    :sparkles: navigation bar

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,9 +6,17 @@ import {
     Link,
     Redirect
 } from "react-router-dom";
-import "./options.scss";
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import SpeedIcon from '@mui/icons-material/Speed';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import BuildIcon from '@mui/icons-material/Build';
+import SettingsIcon from '@mui/icons-material/Settings';
+import InfoIcon from '@mui/icons-material/Info';
 import Popup from './Popup.js';
 import Foreground from './Foreground.js';
+import "./options.scss";
 
 function Options() {
     return (
@@ -22,7 +30,7 @@ function Options() {
                     <div className="header">
 
                         <div className="logo">
-                            <img src="../icons/icon_48.png"></img>
+                            <img src="../icons/icon_128.png"></img>
                         </div>
 
                         <div className="title">
@@ -31,34 +39,54 @@ function Options() {
 
                     </div>
 
-                    <div className="horizontal-line"></div>
+                    <Divider />
 
                     {/* Navigation items */}
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Options</Link>
-                            </li>
-                            <li>
-                                <Link to="/popup">Popup</Link>
-                            </li>
-                            <li>
-                                <Link to="/foreground">Foreground</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <List>
+
+                        <ListItem button key="Dashboard">
+                            <div className="nav-icon"><SpeedIcon/></div>
+                            <Link to="/" className="nav-item">Dashboard</Link>
+                        </ListItem>
+                        <ListItem button key="Learn more">
+                            <div className="nav-icon"><MenuBookIcon/></div>
+                            <Link to="/learn-more" className="nav-item">Learn more</Link>
+                        </ListItem>
+                        <ListItem button key="Controls">
+                            <div className="nav-icon"><BuildIcon/></div>
+                            <Link to="/controls" className="nav-item">Controls</Link>
+                        </ListItem>
+                        <ListItem button key="Settings">
+                            <div className="nav-icon"><SettingsIcon/></div>
+                            <Link to="/settings" className="nav-item">Settings</Link>
+                        </ListItem>
+                        <ListItem button key="About us">
+                            <div className="nav-icon"><InfoIcon/></div>
+                            <Link to="/about-us" className="nav-item">About us</Link>
+                        </ListItem>
+
+                    </List>
+
                 </div>
+
                 <Switch>
-                    <Route exact path="/popup">
-                        <Popup />
-                    </Route>
-                    <Route exact path="/foreground">
-                        <Foreground />
-                    </Route>
                     <Route exact path="/">
                         <Redirect to="/options.html" />
                     </Route>
+                    <Route exact path="/learn-more">
+                        <Redirect to="/options.html" />
+                    </Route>
+                    <Route exact path="/controls">
+                        <Redirect to="/options.html" />
+                    </Route>
+                    <Route exact path="/settings">
+                        <Redirect to="/options.html" />
+                    </Route>
+                    <Route exact path="/about-us">
+                        <Redirect to="/options.html" />
+                    </Route>
                 </Switch>
+
             </div>
         </Router>
     )

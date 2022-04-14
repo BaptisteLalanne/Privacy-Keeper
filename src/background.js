acb12109202a0f6dfb2fbae6cb824935c09a929f
AmL-Dev <aml1234@laposte.net>
2022-04-14 19:19:57 +0200

    :construction: measuring fingerprinting

--- src/background.js
+++ src/background.js
@@ -136,11 +136,51 @@ chrome.runtime.onConnect.addListener(function (port) {
     });
 });
 
-
 const beaconsScript = () => {
+    /**
+     * Returns the number of times strings in an array are present in a text
+     */
+    const findElementFromArrayInText = (array, text) => {
+        var nbAppearance = 0;
+        array.forEach((element) => {
+            if(text.includes(element)){
+                nbAppearance ++;
+            }
+        });
+        return nbAppearance;
+    }
+    
     //This class implements all methods to analyse trackers whithin a web page
     const scripts = document.scripts;
     console.log("Nb of scripts: " + scripts.length);
+
+    // Access to navigator properties
+    var navigatorProperties = ["appCodeName", "appName", "appVersion",
+    "buildID", "cookieEnabled", "doNotTrack",
+    "geolocation", "language", "languages",
+    "onLine", "oscpu", "platform", "product",
+    "productSub", "userAgent", "vendorSub",
+    "vendor", "javaEnabled", "permissions",
+    "mediaDevices", "webdriver", "hardwareConcurrency",
+    "maxTouchPoints", "activeVRDisplays"];
+
+    // Access to Navigator methods
+    var navigatorPropertiesToInstrument = ["vibrate", "sendBeacon", "getGamepads"];
+
+    // Access to plugins
+    var pluginProperties = ["name", "filename", "description", "version", "length"];
+
+    // Access to MIMETypes
+    var mimeTypeProperties = ["description", "suffixes", "type"];
+
+    // Access to Audio API
+    var audioProperties = ["AudioContext", "OfflineAudioContext", "OscillatorNode", "AnalyserNode", "GainNode", "ScriptProcessorNode"];
+
+    let navigatorPropertiesCount = 0;
+    let navigatorPropertiesToInstrumentCount = 0; 
+    let pluginPropertiesCount = 0;
+    let mimeTypePropertiesCount = 0;
+    let audioPropertiesCount = 0;
 
     let nbBeacon = 0;
     for (let i = 0; i < scripts.length; i++) {
@@ -154,8 +194,18 @@ const beaconsScript = () => {
             console.log("FOUND Beacon!")
             nbBeacon++;
         }
+        navigatorPropertiesCount += findElementFromArrayInText(navigatorProperties, scriptContent);
+        navigatorPropertiesToInstrumentCount += findElementFromArrayInText(navigatorPropertiesToInstrument, scriptContent);
+        pluginPropertiesCount += findElementFromArrayInText(pluginProperties, scriptContent);
+        mimeTypePropertiesCount += findElementFromArrayInText(mimeTypeProperties, scriptContent);
+        audioPropertiesCount += findElementFromArrayInText(audioProperties, scriptContent);
     }
     console.log("Nb of beacons found: " + nbBeacon);
+    console.log("navigatorPropertiesCount: " + navigatorPropertiesCount);
+    console.log("navigatorPropertiesToInstrumentCount: " + navigatorPropertiesToInstrumentCount);
+    console.log("pluginPropertiesCount: " + pluginPropertiesCount);
+    console.log("mimeTypePropertiesCount: " + mimeTypePropertiesCount);
+    console.log("audioPropertiesCount: " + audioPropertiesCount);
 
     let port = chrome.runtime.connect({ name: "beacons" });
     port.postMessage({ nb: nbBeacon });

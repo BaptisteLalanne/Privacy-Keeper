b574918d732a4c0ec99f21ebafcc9094195a72d6
Baptiste Lalanne <baptiste.lalanne@outlook.fr>
2022-04-15 10:42:18 +0200

    :sparkles: New architecture

--- src/pages/dashboard/components/Options.js
+++ src/pages/dashboard/components/Options.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {
     BrowserRouter as Router,
-    Switch,
     Route,
     Link,
-    Redirect
 } from "react-router-dom";
 import "./options.scss";
-import Popup from './Popup.js';
-import Foreground from './Foreground.js';
+import Popup from '../../popup/components/Popup.js';
 
 function Options() {
     return (
@@ -24,23 +21,9 @@ function Options() {
                             <li>
                                 <Link to="/popup">Popup</Link>
                             </li>
-                            <li>
-                                <Link to="/foreground">Foreground</Link>
-                            </li>
                         </ul>
                     </nav>
                 </div>
-                <Switch>
-                    <Route exact path="/popup">
-                        <Popup />
-                    </Route>
-                    <Route exact path="/foreground">
-                        <Foreground />
-                    </Route>
-                    <Route exact path="/">
-                        <Redirect to="/options.html" />
-                    </Route>
-                </Switch>
             </div>
         </Router>
     )
